5edce5f215ad996644c97696a7d8855c808cb838
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-09-11 22:17:19 -0300

    Criando Commands

---
+++ Store.Domain/Commands/CreateOrderCommand.cs
@@ -0,0 +1,42 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using Store.Domain.Commands.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Domain.Commands
+{
+    internal class CreateOrderCommand : Notifiable<Notification>, ICommand
+    {
+        public CreateOrderCommand()
+        {
+            Items = new List<CreateOrderItemCommand>();
+        }
+
+        public CreateOrderCommand(string customer, string zipCode, string promoCode, IList<CreateOrderItemCommand> items)
+        {
+            Customer = customer;
+            ZipCode = zipCode;
+            PromoCode = promoCode;
+            Items = items;
+        }
+
+        public string Customer { get; set; }
+        public string ZipCode { get; set; }
+        public string PromoCode { get; set; }
+        public IList<CreateOrderItemCommand> Items { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract<Notification>().Requires()
+                .IsGreaterThan(Customer, 0, "Customer", "Cliente Inválido")
+                .IsBetween(ZipCode.Length, 8, 8, "ZipCode", "CEP Inválido")
+                );
+        }
+    }
+}
---
+++ Store.Domain/Commands/CreateOrderItemCommand.cs
@@ -0,0 +1,30 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using Store.Domain.Commands.Contracts;
+
+namespace Store.Domain.Commands
+{
+    internal class CreateOrderItemCommand : Notifiable<Notification>, ICommand
+    {
+        public CreateOrderItemCommand()
+        {
+        }
+
+        public CreateOrderItemCommand(Guid product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+        public Guid Product;
+        public int Quantity;
+
+        public void Validate()
+        {
+            AddNotifications(new Contract<Notification>()
+                .Requires()
+                .IsGreaterThan(Quantity, 0, "Quantity", "Quantidade Inválida").IsGreaterThan(Product.ToString(), 32, "Product", "Produto Inválido")
+                );
+        }
+    }
+}
