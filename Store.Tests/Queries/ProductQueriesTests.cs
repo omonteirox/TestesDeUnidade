808c773fe8e83c1c8fc6c6f12ab5f89c37ec9248
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-09-11 22:01:59 -0300

    Criando as queries de produto

---
+++ Store.Tests/Queries/ProductQueriesTests.cs
@@ -0,0 +1,34 @@
+﻿using Store.Domain.Queries;
+using Store.Tests.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Tests.Queries
+{
+    [TestClass]
+    public class ProductQueriesTests
+    {
+        private FakeProductRepository _repository = new FakeProductRepository();
+
+        [TestMethod]
+        [TestCategory("Queries")]
+        public void Dado_a_consulta_de_produtos_ativos_deve_retornar_5()
+        {
+            var products = _repository.GetAll();
+            var result = products.AsQueryable().Where(ProductQueries.GetActiveProducts());
+            Assert.AreEqual(5, result.Count());
+        }
+
+        [TestMethod]
+        [TestCategory("Queries")]
+        public void Dado_a_consulta_de_produtos_inativos_deve_retornar_2()
+        {
+            var products = _repository.GetAll();
+            var result = products.AsQueryable().Where(ProductQueries.GetInactiveProducts());
+            Assert.AreEqual(2, result.Count());
+        }
+    }
+}
